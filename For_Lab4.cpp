7624351424e8a11495e0ed4bc24e58a0601583ee
Not-Saint-Paul <145997767+Not-Saint-Paul@users.noreply.github.com>
2023-10-17 03:32:15 +0700

    Update For_Lab4.cpp
    Кто-то это читает?

--- For_Lab4.cpp
+++ For_Lab4.cpp
@@ -1,8 +1,8 @@
 /************************************
  * Автор: Станковский П.А.          *
  * Дата: 17.10.2023                 *
- * Название: Shitcode Lab4 v1       *
- * https://onlinegdb.com/V2roYe2lw  *
+ * Название: Shitcode Lab4 v1.1     *
+ * https://onlinegdb.com/MmUSj3ouO  *
  ************************************/
 #include <iostream>
 #include <math.h>
@@ -10,8 +10,14 @@ using namespace std;
 
 int main() {
     
-    int n, i, j;
-    int B[n][n];
+    setlocale(LC_ALL, "Russian");
+    
+    int n = 3, i, j;
+    int B[n][n] = {
+    {1, 2, 3},
+    {5, -6, -2},
+    {-3, 0, -9}
+    };
     double numberOfNegative, smallestOfNegative;
     
     for (i = 0; i < n; ++i) {
@@ -19,13 +25,22 @@ int main() {
             if (B[i][j] < 0) {
                 numberOfNegative += 1;
                 if (B[i][j] < smallestOfNegative) {
-                smallestOfNegative = B[i][j];
+                    smallestOfNegative = B[i][j];
                 }
             }
         }
     }
-    
-    cout << "numberOfNegative = "  << numberOfNegative << " smallestOfNegative =" << smallestOfNegative << endl;
+
+    cout << "Исходная матрица: " << endl;
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n; ++j) {
+            cout << B[i][j] << " ";
+        }
+        cout << endl;
+    }  
+
+    cout << endl << "Количество отрицательных чисел равно: "  << numberOfNegative << endl 
+    << "Наименьшее из отрицательных чисел равно: " << smallestOfNegative << endl;
 
     return 0;
 }
